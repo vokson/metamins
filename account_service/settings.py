06bc5ffb31625ae80324319205c3779db8618db4
Noskov Alexey <vokson@yandex.ru>
2021-01-15 16:14:06 +0300

    Change to debug mode

--- account_service/settings.py
+++ account_service/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
