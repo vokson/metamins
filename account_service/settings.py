e495362bc8aad82333d7d04d7d8eb97bb4d3bc64
Noskov Alexey <vokson@yandex.ru>
2021-01-17 07:43:11 +0300

    Update settings.py

--- account_service/settings.py
+++ account_service/settings.py
@@ -66,7 +66,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'account_service.wsgi.application'
 
 DATABASES = {
-    'default': env.db()
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
